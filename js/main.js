93dd4f91e37b392017cf5b0546b2eef926febe9f
Emam Ul Haque Emon <emam5316@gmail.com>
2022-10-12 03:06:59 +0600

    e

--- js/main.js
+++ js/main.js
@@ -1,16 +1,10 @@
 function openNav() {
     document.getElementById('sidenav').style.width = "250px";
-
 }
 
 function closeNav() {
     document.getElementById('sidenav').style.width = "0";
-
 }
-
-
-
-
 let valueDisplays = document.querySelectorAll(".num");
 let interval = 5000;
 valueDisplays.forEach((valueDisplays) => {
@@ -24,20 +18,4 @@ valueDisplays.forEach((valueDisplays) => {
             clearInterval(counter);
         }
     }, duration);
-
-
 });
-// valueDisplays.forEach((valueDisplay) => {
-//     let startValue = 0;
-//     let endValue = parseInt(valueDisplays.getAttribute("data-val"));
-//     let duration = math.floor(interval / endValue);
-//     let counter = setInterval(function() {
-
-//         startValue += 1;
-//         valueDisplay.textContent = startValue;
-//         if (startValue == endValue) {
-//             clearInterval(counter)
-//         }
-//     }, duration);
-
-// });
