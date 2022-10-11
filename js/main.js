f9708367e49263e99500a6d65d5e993411894d99
Emam Ul Haque Emon <emam5316@gmail.com>
2022-10-12 01:59:48 +0600

    d

--- js/main.js
+++ js/main.js
@@ -7,3 +7,37 @@ function closeNav() {
     document.getElementById('sidenav').style.width = "0";
 
 }
+
+
+
+
+let valueDisplays = document.querySelectorAll(".num");
+let interval = 5000;
+valueDisplays.forEach((valueDisplays) => {
+    let startValue = 0;
+    let endValue = parseInt(valueDisplays.getAttribute("data-val"));
+    let duration = Math.floor(interval / endValue);
+    let counter = setInterval(function() {
+        startValue += 1;
+        valueDisplays.textContent = startValue;
+        if (startValue == endValue) {
+            clearInterval(counter);
+        }
+    }, duration);
+
+
+});
+// valueDisplays.forEach((valueDisplay) => {
+//     let startValue = 0;
+//     let endValue = parseInt(valueDisplays.getAttribute("data-val"));
+//     let duration = math.floor(interval / endValue);
+//     let counter = setInterval(function() {
+
+//         startValue += 1;
+//         valueDisplay.textContent = startValue;
+//         if (startValue == endValue) {
+//             clearInterval(counter)
+//         }
+//     }, duration);
+
+// });
